dd9225070a7ab83c5ecc3d42fa131a31d8b7d5a7
rgsone <rgs@rgsone.com>
2023-02-05 16:25:05 +0100

    add favicons, etc.

--- src/app.js
+++ src/app.js
@@ -1,2 +1,28 @@
 import 'bootstrap/dist/css/bootstrap-reboot.css';
 import './styles/app.css';
+import {registerWindowHeight} from './utils/window.js';
+import {fontIsLoaded} from "./utils/font.js";
+
+function launch() {
+
+	// get and set viewport height css var
+	registerWindowHeight();
+	// wait for font loading
+	fontIsLoaded();
+
+	//const canvasElem = document.getElementById('canvas');
+	//const canvasApp = new CanvasApp(canvasElem);
+
+	// app is ready
+	setTimeout(() => {
+		document.documentElement.classList.add('ready');
+	}, 1200);
+}
+
+////////////////////////////////////////////////////
+
+if (document.readyState === 'loading') {
+	document.addEventListener('DOMContentLoaded', launch);
+} else {
+	launch();
+}
